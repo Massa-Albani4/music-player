c09ed166f821dc8511751e466bdd170782bfd110
Massa-Albani4 <massa.albani0213@gmail.com>
2024-08-26 16:38:14 +0300

    Add design system

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bwquintapro: ["bwquintapro", "sans-serif"],
+      },
+    },
+    colors: {
+      white: "#F8F8FF",
+      black: "#313638",
+      gray: "#D8D8D8",
+      orange: "#EE6C4D",
+    },
+  },
+  plugins: [],
+};
