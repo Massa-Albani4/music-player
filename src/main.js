cf4fd02a324b1c3ed8e6c102c7e624060aeec7e6
Massa-Albani4 <massa.albani0213@gmail.com>
2024-09-03 18:45:40 +0300

    Add elements id

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 const musicContainer = document.getElementById("music-container");
-const playBtn = document.getElementById("");
-const pauseBtn = document.getElementById("music-container");
-const previousBtn = document.getElementById("music-container");
-const nextBtn = document.getElementById("music-container");
+const playBtn = document.getElementById("play-btn");
+const previousBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress-bar");
+const progressContainer = document.getElementById("progress-container");
+const Title = document.getElementById("song-title");
+const cover = document.getElementById("song-cover");
+
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,7 @@ module.exports = {
       Montserrat: ["Montserrat", "sans-serif"],
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
 };
