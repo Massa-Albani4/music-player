27c27b6b70b72380c45bec3c885d0b4ed10dec96
Massa Albani <massa.albani0213@gmail.com>
2025-11-15 03:43:31 +0400

    Update elements size for laptops

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,18 @@ module.exports = {
         orange: "#E9873C",
         gray: "#191919",
         black: "#020202",
-        "gray-500": "#2F2F2F", 
+        "gray-500": "#2F2F2F",
         "gray-400": "#878787",
-        white: "#F8F8FF"
+        white: "#F8F8FF",
       },
       screens: {
-        xl: "1920px",
         sm: "432px",
         lg: "1440px",
-      }
+      },
     },
     fontFamily: {
       Montserrat: ["Montserrat", "sans-serif"],
-    }
+    },
   },
   plugins: [],
 };
